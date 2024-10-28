4e24cc5228f7cd143fcfb9c68d9716871460fceb
Cuong Jackfruit <taduccuong2003@gmail.com>
2024-10-28 13:49:14 +0700

    Update

--- Candidate_Repositories/HRAccountRepo.cs
+++ Candidate_Repositories/HRAccountRepo.cs
@@ -6,14 +6,7 @@ namespace Candidate_Repositories
 {
     public class HrAccountRepo : IHRAccountRepo
     {
-
-        public HrAccountRepo()
-        {
-
-        }
         public Hraccount GetHraccountByEmail(string email) => HRAccountDAO.Instance.GetHraccountByEmail(email);
-
-     
         public List<Hraccount> GetHraccounts() => HRAccountDAO.Instance.GetHraccounts();
 
     }
